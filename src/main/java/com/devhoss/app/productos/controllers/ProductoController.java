03194f8d987534b284b36f4f2e6a16c8e5ebe8d8
devhoss <velasco2288@gmail.com>
2023-09-11 17:28:31 -0500

    SINC2 - load balancer

--- src/main/java/com/devhoss/app/productos/controllers/ProductoController.java
+++ src/main/java/com/devhoss/app/productos/controllers/ProductoController.java
@@ -16,23 +16,30 @@ import com.devhoss.app.productos.services.IProductoService;
 @RestController
 public class ProductoController {
 	
-	    // @Autowired
-		//private Environment env;
+	     @Autowired
+		private Environment env;
 		
-		//@Value("${server.port}")
-		//private Integer port;
+		@Value("${server.port}")
+		private Integer port;
 		
 		@Autowired
 		private IProductoService productoService;
 		
 		@GetMapping("/listar")
 		public List<Producto> listar(){
-			return productoService.findAll();
+			return productoService.findAll().stream().map(producto ->{
+				producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+				//producto.setPort(port);
+				return producto;
+			}).collect(Collectors.toList());
 		}
 		
 		@GetMapping("/ver/{id}")
 		public Producto detalle(@PathVariable Long id) {
-			return productoService.findById(id);
+			Producto producto = productoService.findById(id);
+			producto.setPort(Integer.parseInt(env.getProperty("local.server.port")));
+			//producto.setPort(port);
+			return producto;
 		}
 
 }
