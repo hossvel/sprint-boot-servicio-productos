fbddb3854a502bb994366179cf34276d4ad7fa6c
devhoss <velasco2288@gmail.com>
2023-09-11 10:20:59 -0500

    proyecto inicial

---
+++ src/test/java/com/devhoss/app/productos/SprintBootServicioProductosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devhoss.app.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootServicioProductosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
